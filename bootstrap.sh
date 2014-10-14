3bee7dd0e332e696bf5ceffa3404ea8b5244a0be
=ola adolfsson <=ola.adolfsson@softhouse.se>
2014-10-14 14:53:07 +0300

    Added programs: Java 8 WebStorm gradle 1.x Eclipse med gradle-plugin Python3 med pip och virtualenv Google chrome

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,43 +1,72 @@
 #!/usr/bin/env bash
 
 webstorm_url="http://download.jetbrains.com/webstorm/WebStorm-8.0.4.tar.gz"
+gradle_url="https://services.gradle.org/distributions/gradle-1.12-bin.zip"
+program_path="/home/vagrant/program"
+download_path="/vagrant/.download"
+temp_path="~/temp"
+test="ftp://ftp.sunet.se/pub/www/utilities/curl/curl-7.38.0.tar.gz"
 
-#Install java8
+# constants
+gradle_version=1.12
+
+# Help functions
+
+function file_download()
+{
+        wget $1 -N -P "$download_path"
+        dest_path="$program_path"/$2
+        mkdir -p "$dest_path"
+
+        case "$3" in
+                untar)
+                /bin/tar xzf "$download_path/$(basename "$1")" -C "$dest_path"
+                ;;
+
+                unzip)
+                /usr/bin/unzip "$download_path/$(basename "$1")"  -d "$dest_path"
+                ;;
+
+                *)
+                echo  "$3 not supported"
+        esac
+
+}
+
+function install_gradle() {
+        mkdir /opt/gradle
+        wget -N http://services.gradle.org/distributions/gradle-${gradle_version}-all.zip -P "$download_path" 
+        unzip -oq $download_path/gradle-${gradle_version}-all.zip -d /opt/gradle
+        ln -sfnv /opt/gradle/gradle-${gradle_version} /opt/gradle/latest
+        printf "export GRADLE_HOME=/opt/gradle/latest\nexport PATH=\$PATH:\$GRADLE_HOME/bin" > /etc/profile.d/gradle.sh
+        . /etc/profile.d/gradle.sh
+        chmod +x /etc/profile.d/gradle.sh
+}
+
+# Main program
+
+# Install java8
 add-apt-repository -y ppa:webupd8team/java 
 apt-get -y update
 echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
 apt-get -qq -y install oracle-java8-installer oracle-java8-set-default
 
+# Install WebStorm
+file_download "${webstorm_url}" "webstorm"
 
+#Install gradle
+#file_download "${gradle_url}" "gradle"
+#echo 'export PATH=${PATH}:"${program_path}/gradle"' >> ${HOME}/.bash_profile
 
-#Install WebStorm
-if [ ! -d "/vagrant/.download/`basename \"${webstorm_url}\"`" ] ; then
-	mkdir -vp /vagrant/.download/
-	wget --quiet "${webstorm_url}" -P /vagrant/.download/
-fi
+#Install eclipse
+sudo apt-get install eclipse
 
+#Install eclipse gradle plugin
+eclipse -nosplash -application org.eclipse.equinox.p2.director -repository http://dist.springsource.com/release/TOOLS/gradle -installIU org.springsource.ide.eclipse.gradle.feature.feature.group
 
-#if [ ! -d "${HOME}/android-sdk-linux/" ] ; then
-	
-	
-	#echo 'export ANDROID_SDK_HOME=/vagrant/' >> ${HOME}/.bash_profile
-	#echo "export ANDROID_HOME=${HOME}/android-sdk-linux" >> ${HOME}/.bash_profile
-	#echo 'export PATH=${PATH}:${ANDROID_HOME}/platform-tools:${ANDROID_HOME}/tools' >> ${HOME}/.bash_profile
-	#echo 'export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386' >> ${HOME}/.bash_profile
-	
-	#source ${HOME}/.bash_profile
+#Install python with PIP and VirtualEnv
+sudo apt-get install python-pip python-virtualenv
 
-	#mkdir -vp ${ANDROID_SDK_HOME}
-	#mkdir -vp /vagrant/.download/
-	#if [ ! -f "/vagrant/.download/${android_sdk}" ] ; then
-	#	echo "downloading ${android_sdk_url}"
-	#	wget --quiet "${android_sdk_url}" -P /vagrant/.download/
-	#fi
-
-	tar -xvzf /vagrant/.download/${android_sdk}
-#fi
-
-
-apt-get -y install git
-
+#Install chrome
+sudo apt-get install chromium-browser
