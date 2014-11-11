1b2f4c380e12cd1c2c7c019bc16e39b73120170d
xandrto <andreas.tornstrom.andersson@softhouse.se>
2014-11-11 21:20:26 +0100

    Added mongo

---
+++ .bootstrap/mongo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org=2.6.1 mongodb-org-server=2.6.1 mongodb-org-shell=2.6.1 mongodb-org-mongos=2.6.1 mongodb-org-tools=2.6.1
+
+sudo sed -i -e 's/^bind_ip.*/bind_ip = 0.0.0.0/' /etc/mongod.conf
+
+sudo service mongod restart
